52059462c42fc97be1ab6dbd45db74028833dd5d
Devesh Patel <deveshp530@gmail.com>
2020-05-18 15:10:41 -0700

    many-to-many

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+
     #call to find new stock
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.stock_api_key[:publisher_key],
